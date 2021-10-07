031b37c5718a055e47247e1beb5154462ada6e94
teddys31 <teddy.stanowski@hdsupply.com>
2021-10-07 10:43:09 -0700

    call posts in an index json file and render in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
+<script context="module">
+	// import { client } from '$lib/graphql-client';
+	// import { gql } from 'graphql-request';
+
+	export async function load({ fetch }) {
+		const res = await fetch('/posts.json');
+		if (res.ok) {
+			const posts = await res.json();
+			return { props: posts };
+		}
+	}
+</script>
+
+<script>
+	export let posts;
+	console.log(posts);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/posts/index.json.js
@@ -0,0 +1,30 @@
+import { client } from "$lib/graphql-client";
+import { gql } from "graphql-request";
+
+export const get = async (req, res) =>
+{
+  try {
+		const query = gql`
+			query Post {
+				posts {
+					title
+					slug
+					date
+					excerpt
+					tags
+					coverImage {
+						url
+					}
+				}
+			}
+		`;
+    const { posts } = await client.request(query);
+    return {status:200, body:{posts}}
+  } catch (error)
+  {
+    return {
+      status: 500,
+      body:{error:error.message}
+    }
+  }
+}
