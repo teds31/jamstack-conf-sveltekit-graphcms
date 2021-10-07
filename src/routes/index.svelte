5fadd5f27be12caaa73a002d5446930a27ad549c
teddys31 <teddy.stanowski@hdsupply.com>
2021-10-07 11:12:21 -0700

    render posts and add styling with tailwind and daisy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,4 @@
 <script context="module">
-	// import { client } from '$lib/graphql-client';
-	// import { gql } from 'graphql-request';
-
 	export async function load({ fetch }) {
 		const res = await fetch('/posts.json');
 		if (res.ok) {
@@ -13,8 +10,36 @@
 
 <script>
 	export let posts;
-	console.log(posts);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Teddy's Jamstack Conf Blog</title>
+</svelte:head>
+
+<ul>
+	{#each posts as post}
+		<li class="card text-center shadow-2xl mb-20">
+			<figure class="px-10 pt-10">
+				{#if post.coverImage.url}
+					<img src={post.coverImage.url} alt={post.title} />
+				{:else}
+					<img src={`/default-cover-image.jpg`} alt="default cover image" />
+				{/if}
+				<h2 class="title font-bold text-2l mb-2">
+					{post.title}
+				</h2>
+				<p class="mb-2">{post.excerpt}</p>
+				<div>
+					{#if post.tags}
+						{#each post.tags as tag}
+							<span class="badge badge-primary">{tag}</span>
+						{/each}
+					{/if}
+				</div>
+				<div class="my-2">
+					<a href={`/posts/${post.slug}`} class="btn">Read &rArr</a>
+				</div>
+			</figure>
+		</li>
+	{/each}
+</ul>
